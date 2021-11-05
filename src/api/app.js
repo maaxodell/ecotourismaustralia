6fe68df4e956b12111092768d614a29fb7e77c3b
PC <maxodell11@gmail.com>
2021-11-05 19:34:41 +1000

    cleaned up api

--- src/api/app.js
+++ src/api/app.js
@@ -3,10 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//require("./database").connect();
 var cors = require('cors');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -22,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
